b1c9e5596ab883e4efe73bd02ccf4397589ae8c6
Patrícia <pg.costa@campus.fct.unl.pt>
2025-10-02 17:24:06 +0100

    css modules and nested routes

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -11,6 +13,13 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
